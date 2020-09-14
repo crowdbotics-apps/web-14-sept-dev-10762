c5e94bd16adab0c9ca8d713756ebabc791a519ea
Crowdbotics <team@crowdbotics.com>
2020-09-14 14:15:57 +0000

    Update components

--- src/features/CopyOfBlankScreen210791/screens/index.js
+++ src/features/CopyOfBlankScreen210791/screens/index.js
@@ -19,7 +19,7 @@ export class __Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
-  state = {}
+  state = { Datepicker_2: new Date("09/14/2020") }
   render = () => (
     <View
       style={{
@@ -53,7 +53,42 @@ export class __Blank extends React.Component {
         borderRadius: 0,
         backgroundSize: "auto"
       }}
-    />
+    >
+      <Datepicker
+        style={{
+          width: "100%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          fontSize: 12,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontFamily: "Roboto-Regular",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textAlign: "left",
+          textAlignVertical: "center",
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+        date={this.state.Datepicker_2}
+        onSelect={nextValue => this.setState({ Datepicker_2: nextValue })}
+      />
+    </View>
   )
 }
 
